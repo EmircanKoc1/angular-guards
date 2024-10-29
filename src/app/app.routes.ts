0560dd741fc35e8883b1d589de1a83b743e1f245
Emircan <emircandv787@gmail.com>
2024-10-29 09:57:38 +0300

    The routes has been added and profile component has been refactored.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,29 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileMyinformationComponent } from './components/profile-myinformation/profile-myinformation.component';
+import { isLoginGuard } from './guards/is-login.guard';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
         path: "profile",
         component: ProfileComponent,
-        canActivate: []
+        canActivate: [isLoginGuard],
+        children: [
+            {
+                path: "",
+                redirectTo: "my-information",
+                pathMatch: "full"
+            },
+            {
+                path: "my-information",
+                component: ProfileMyinformationComponent
+            }
+        ]
     },
+    {
+        path: "login",
+        component: LoginComponent
+    }
 
 ];
