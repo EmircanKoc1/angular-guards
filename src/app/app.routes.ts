b23571db4bc073bb8e33554987dfa27144889ddb
Emircan <emircandv787@gmail.com>
2024-10-29 09:12:23 +0300

    the example guard has been deleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { exampleGuard } from './guards/example.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "profile",
+        component: ProfileComponent,
+        canActivate: [exampleGuard]
+    }
+];
---
+++ src/app/guards/example.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const exampleGuard: CanActivateFn = (route, state) => {
+
+
+  // return true;
+  return false;
+
+};
